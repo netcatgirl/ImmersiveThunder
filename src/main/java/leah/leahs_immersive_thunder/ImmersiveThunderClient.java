33f467063ae8a0f820412dc275737a0a3f5a4b4a
aveniir <leahniederpruem@proton.me>
2023-03-24 12:54:52 +0100

    Change formatting

--- src/main/java/leah/leahs_immersive_thunder/ImmersiveThunderClient.java
+++ src/main/java/leah/leahs_immersive_thunder/ImmersiveThunderClient.java
@@ -10,23 +10,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ImmersiveThunderClient implements ModInitializer {
-    public static final String MOD_ID = "leahs-immersive-thunder";
-    public static final Logger LOGGER = LoggerFactory.getLogger("leahs-immersive-thunder");
+  public static final String MOD_ID = "leahs-immersive-thunder";
+  public static final Logger LOGGER = LoggerFactory.getLogger("leahs-immersive-thunder");
 
-    public static final Identifier THUNDER_CLOSE = new Identifier("leahs-immersive-thunder:thunder_close");
-    public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_CLOSE = SoundEvent.of(THUNDER_CLOSE);
+  public static final Identifier THUNDER_CLOSE = new Identifier("leahs-immersive-thunder:thunder_close");
+  public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_CLOSE = SoundEvent.of(THUNDER_CLOSE);
 
-    public static final Identifier THUNDER_MEDIUM = new Identifier("leahs-immersive-thunder:thunder_medium");
-    public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_MEDIUM = SoundEvent.of(THUNDER_MEDIUM);
+  public static final Identifier THUNDER_MEDIUM = new Identifier("leahs-immersive-thunder:thunder_medium");
+  public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_MEDIUM = SoundEvent.of(THUNDER_MEDIUM);
 
-    public static final Identifier THUNDER_FAR = new Identifier("leahs-immersive-thunder:thunder_far");
-    public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_FAR = SoundEvent.of(THUNDER_FAR);
+  public static final Identifier THUNDER_FAR = new Identifier("leahs-immersive-thunder:thunder_far");
+  public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_FAR = SoundEvent.of(THUNDER_FAR);
 
-    @Override
-    public void onInitialize() {
-        Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_CLOSE, ENTITY_LIGHTNING_BOLT_THUNDER_CLOSE);
-        Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_MEDIUM, ENTITY_LIGHTNING_BOLT_THUNDER_MEDIUM);
-        Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_FAR, ENTITY_LIGHTNING_BOLT_THUNDER_FAR);
-        LOGGER.info("Immersive Thunder loaded.");
-    }
+  @Override
+  public void onInitialize() {
+    Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_CLOSE, ENTITY_LIGHTNING_BOLT_THUNDER_CLOSE);
+    Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_MEDIUM, ENTITY_LIGHTNING_BOLT_THUNDER_MEDIUM);
+    Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_FAR, ENTITY_LIGHTNING_BOLT_THUNDER_FAR);
+    LOGGER.info("Immersive Thunder loaded.");
+  }
 }
