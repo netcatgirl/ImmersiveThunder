82d678c36f52483f5ba74f23c4a867c12bdbcd41
Leah <leahniederpruem@proton.me>
2023-07-06 16:37:28 +0200

    Add range constraints

--- src/main/java/leah/leahs_immersive_thunder/ImmersiveThunderConfigClass.java
+++ src/main/java/leah/leahs_immersive_thunder/ImmersiveThunderConfigClass.java
@@ -2,14 +2,21 @@ package leah.leahs_immersive_thunder;
 
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
+import io.wispforest.owo.config.annotation.RangeConstraint;
 
 @Modmenu(modId = ImmersiveThunderClient.MOD_ID)
 @Config(name = "immersive-thunder-config", wrapperName = "ImmersiveThunderConfig")
 public class ImmersiveThunderConfigClass {
+    @RangeConstraint(min = 0f, max = 5000.0f)
     public float thunderCloseVolume = 5000.0f;
+
+    @RangeConstraint(min = 0f, max = 15000.0f)
     public float thunderMediumVolume = 10000.0f;
+
+    @RangeConstraint(min = 0f, max = 15000.0f)
     public float thunderFarVolume = 10000.0f;
 
     public boolean impactSound = true;
+    @RangeConstraint(min = 0f, max = 5.0f)
     public float impactSoundVolume = 2.0f;
 }
