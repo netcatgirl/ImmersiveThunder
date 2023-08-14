cba4a3e8e5ece7b1d680eaef31daa15291a77f03
Leah <leahniederpruem@proton.me>
2023-08-14 15:07:09 +0200

    Fix formatting

--- src/main/java/leah/leahs_immersive_thunder/ImmersiveThunderClient.java
+++ src/main/java/leah/leahs_immersive_thunder/ImmersiveThunderClient.java
@@ -13,28 +13,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ImmersiveThunderClient implements ModInitializer {
-  public static final String MOD_ID = "leahs-immersive-thunder";
-  public static final Logger LOGGER = LoggerFactory.getLogger("leahs-immersive-thunder");
+    public static final String MOD_ID = "leahs-immersive-thunder";
+    public static final Logger LOGGER = LoggerFactory.getLogger("leahs-immersive-thunder");
 
-  public static ConfigHolder<ImmersiveThunderConfig> configHolder;
+    public static ConfigHolder<ImmersiveThunderConfig> configHolder;
 
-  public static final Identifier THUNDER_CLOSE = new Identifier("leahs-immersive-thunder:thunder_close");
-  public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_CLOSE = SoundEvent.of(THUNDER_CLOSE);
+    public static final Identifier THUNDER_CLOSE = new Identifier("leahs-immersive-thunder:thunder_close");
+    public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_CLOSE = SoundEvent.of(THUNDER_CLOSE);
 
-  public static final Identifier THUNDER_MEDIUM = new Identifier("leahs-immersive-thunder:thunder_medium");
-  public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_MEDIUM = SoundEvent.of(THUNDER_MEDIUM);
+    public static final Identifier THUNDER_MEDIUM = new Identifier("leahs-immersive-thunder:thunder_medium");
+    public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_MEDIUM = SoundEvent.of(THUNDER_MEDIUM);
 
-  public static final Identifier THUNDER_FAR = new Identifier("leahs-immersive-thunder:thunder_far");
-  public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_FAR = SoundEvent.of(THUNDER_FAR);
+    public static final Identifier THUNDER_FAR = new Identifier("leahs-immersive-thunder:thunder_far");
+    public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_FAR = SoundEvent.of(THUNDER_FAR);
 
-  @Override
-  public void onInitialize() {
-    configHolder = AutoConfig.register(ImmersiveThunderConfig.class, Toml4jConfigSerializer::new);
+    @Override
+    public void onInitialize() {
+        configHolder = AutoConfig.register(ImmersiveThunderConfig.class, Toml4jConfigSerializer::new);
 
-    Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_CLOSE, ENTITY_LIGHTNING_BOLT_THUNDER_CLOSE);
-    Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_MEDIUM, ENTITY_LIGHTNING_BOLT_THUNDER_MEDIUM);
-    Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_FAR, ENTITY_LIGHTNING_BOLT_THUNDER_FAR);
+        Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_CLOSE, ENTITY_LIGHTNING_BOLT_THUNDER_CLOSE);
+        Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_MEDIUM, ENTITY_LIGHTNING_BOLT_THUNDER_MEDIUM);
+        Registry.register(Registries.SOUND_EVENT, ImmersiveThunderClient.THUNDER_FAR, ENTITY_LIGHTNING_BOLT_THUNDER_FAR);
 
-    LOGGER.info("Immersive Thunder loaded.");
-  }
+        LOGGER.info("Immersive Thunder loaded.");
+    }
 }
--- src/main/java/leah/leahs_immersive_thunder/mixin/ImmersiveThunderMixin.java
+++ src/main/java/leah/leahs_immersive_thunder/mixin/ImmersiveThunderMixin.java
@@ -14,35 +14,36 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
 import java.util.Objects;
 
 @Mixin(LightningEntity.class)
 public class ImmersiveThunderMixin implements ThunderSoundInterface {
-  @Unique
-  ImmersiveThunderConfig config = ImmersiveThunderClient.configHolder.getConfig();
+    @Unique
+    ImmersiveThunderConfig config = ImmersiveThunderClient.configHolder.getConfig();
 
-  @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playSound(DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FFZ)V"))
-  private void playSound(World world, double x, double y, double z, SoundEvent sound, SoundCategory category, float volume, float pitch, boolean useDistance) {
-    LightningEntity lightning = (LightningEntity) (Object) this;
-    PlayerEntity player = MinecraftClient.getInstance().player;
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playSound(DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FFZ)V"))
+    private void playSound(World world, double x, double y, double z, SoundEvent sound, SoundCategory category, float volume, float pitch, boolean useDistance) {
+        LightningEntity lightning = (LightningEntity) (Object) this;
+        PlayerEntity player = MinecraftClient.getInstance().player;
 
-    double distanceToEntity = Objects.requireNonNull(player).distanceTo(lightning);
+        double distanceToEntity = Objects.requireNonNull(player).distanceTo(lightning);
 
-    if (distanceToEntity <= closeDistance) {
-      playThunderSound(world, lightning, ImmersiveThunderClient.ENTITY_LIGHTNING_BOLT_THUNDER_CLOSE, thunderCloseVolume, false);
-    } else if (distanceToEntity <= mediumDistance) {
-      playThunderSound(world, lightning, ImmersiveThunderClient.ENTITY_LIGHTNING_BOLT_THUNDER_MEDIUM, thunderMediumVolume, true);
-    } else {
-      playThunderSound(world, lightning, ImmersiveThunderClient.ENTITY_LIGHTNING_BOLT_THUNDER_FAR, thunderFarVolume, true);
+        if (distanceToEntity <= closeDistance) {
+            playThunderSound(world, lightning, ImmersiveThunderClient.ENTITY_LIGHTNING_BOLT_THUNDER_CLOSE, thunderCloseVolume, false);
+        } else if (distanceToEntity <= mediumDistance) {
+            playThunderSound(world, lightning, ImmersiveThunderClient.ENTITY_LIGHTNING_BOLT_THUNDER_MEDIUM, thunderMediumVolume, true);
+        } else {
+            playThunderSound(world, lightning, ImmersiveThunderClient.ENTITY_LIGHTNING_BOLT_THUNDER_FAR, thunderFarVolume, true);
+        }
     }
-  }
 
-  @Override
-  public void playThunderSound(World world, LightningEntity lightning, SoundEvent soundEvent, float volume, boolean useDistance) {
-    world.playSound(lightning.getX(), lightning.getY(), lightning.getZ(), soundEvent, SoundCategory.WEATHER, volume, 0.8f + world.random.nextFloat() * 0.2f, useDistance);
+    @Override
+    public void playThunderSound(World world, LightningEntity lightning, SoundEvent soundEvent, float volume, boolean useDistance) {
+        world.playSound(lightning.getX(), lightning.getY(), lightning.getZ(), soundEvent, SoundCategory.WEATHER, volume, 0.8f + world.random.nextFloat() * 0.2f, useDistance);
 
-    if (config.impactSound) {
-      world.playSound(lightning.getX(), lightning.getY(), lightning.getZ(), SoundEvents.ENTITY_LIGHTNING_BOLT_IMPACT, SoundCategory.WEATHER, impactSoundVolume, 0.5f + world.random.nextFloat() * 0.2f, false);
+        if (config.impactSound) {
+            world.playSound(lightning.getX(), lightning.getY(), lightning.getZ(), SoundEvents.ENTITY_LIGHTNING_BOLT_IMPACT, SoundCategory.WEATHER, impactSoundVolume, 0.5f + world.random.nextFloat() * 0.2f, false);
+        }
     }
-  }
 }
