2f95e70b80803a94c15cbfa318240f5c25a863af
Leah <leahniederpruem@proton.me>
2023-08-14 14:33:47 +0200

    Add default values

--- src/main/java/leah/leahs_immersive_thunder/util/ThunderSoundInterface.java
+++ src/main/java/leah/leahs_immersive_thunder/util/ThunderSoundInterface.java
@@ -8,5 +8,10 @@ public interface ThunderSoundInterface {
   double closeDistance = 90;
   double mediumDistance = 140;
 
+  float thunderCloseVolume = 5000.0f;
+  float thunderMediumVolume = 10000.0f;
+  float thunderFarVolume = 10000.0f;
+  float impactSoundVolume = 2.0f;
+
   void playThunderSound(World world, LightningEntity lightning, SoundEvent soundEvent, float volume, boolean useDistance);
 }
