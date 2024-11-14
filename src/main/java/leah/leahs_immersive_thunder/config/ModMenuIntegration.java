4c82307060989a6365956ff246e9efcd2f48b7fe
Leah <root@leah.sh>
2024-11-14 13:50:07 +0100

    Update to 1.21.2

--- src/main/java/leah/leahs_immersive_thunder/config/ModMenuIntegration.java
+++ src/main/java/leah/leahs_immersive_thunder/config/ModMenuIntegration.java
@@ -2,6 +2,7 @@ package leah.leahs_immersive_thunder.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import leah.leahs_immersive_thunder.config.ImmersiveThunderConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 
 public class ModMenuIntegration implements ModMenuApi {
