a8ce140e044bea37d16fbadcc12e739ae6f8f0e9
Leah <root@leah.sh>
2024-11-14 13:54:34 +0100

    Remove unused import

--- src/main/java/leah/leahs_immersive_thunder/config/ModMenuIntegration.java
+++ src/main/java/leah/leahs_immersive_thunder/config/ModMenuIntegration.java
@@ -2,7 +2,6 @@ package leah.leahs_immersive_thunder.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import leah.leahs_immersive_thunder.config.ImmersiveThunderConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 
 public class ModMenuIntegration implements ModMenuApi {
